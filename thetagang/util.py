0ebb8b4d1afb869c9442852ed6e8366a72b14a5d
Brenden Matthews <brenden@brndn.io>
2021-02-05 10:32:54 -0600

    Raise exception on timeout waiting for data.

--- thetagang/util.py
+++ thetagang/util.py
@@ -53,6 +53,8 @@ def count_option_positions(symbol, portfolio_positions, right):
 
 
 def while_n_times(pred, body, remaining):
+    if remaining <= 0:
+        raise "Timeout waiting on predicate. This shouldn't happen. "
     if pred() and remaining > 0:
         body()
         while_n_times(pred, body, remaining - 1)
