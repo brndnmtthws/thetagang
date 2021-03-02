860a819464fee8e243aa99478da3a83de93804c6
Brenden Matthews <brenden@brndn.io>
2021-03-02 13:45:56 -0600

    Fix raising exception.

--- thetagang/util.py
+++ thetagang/util.py
@@ -54,7 +54,7 @@ def count_option_positions(symbol, portfolio_positions, right):
 
 def while_n_times(pred, body, remaining):
     if remaining <= 0:
-        raise "Exhausted retries waiting on predicate. This shouldn't happen. "
+        raise RuntimeError("Exhausted retries waiting on predicate. This shouldn't happen.")
     if pred() and remaining > 0:
         body()
         while_n_times(pred, body, remaining - 1)
