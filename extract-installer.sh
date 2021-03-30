96b642f56eaf6a547d85e4aec0dfb48c2a97af9e
Brenden Matthews <brenden@brndn.io>
2021-03-30 09:37:45 -0500

    Update tws hash.

--- extract-installer.sh
+++ extract-installer.sh
@@ -7,7 +7,7 @@ mkdir -p tws
 docker run -i --rm -v `pwd`/tws:/tws debian sh -c " \
     apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -qy --no-install-recommends unzip curl ca-certificates \
-    && echo '09a2d39a1f5727346f43959b6c52b7becd1e4dd42b5ab907dd7d29cd51a023df  tws-installer.sh' | tee tws-installer.sh.sha256 \
+    && echo '75f2c2071bfc342932d6bca4ec321783ecb115cb99d23290fa254a2b87200c63  tws-installer.sh' | tee tws-installer.sh.sha256 \
     && curl -qL https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-linux-x64.sh -o tws-installer.sh \
     && sha256sum -c tws-installer.sh.sha256 \
     && yes "" | sh tws-installer.sh \
