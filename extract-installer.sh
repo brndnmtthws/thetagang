a268b0c431f54b831aa66cc2dc7ba1425cef16dd
Brenden Matthews <brenden@brndn.io>
2021-06-12 11:32:06 -0500

    Remove uninstall file as it messes things up.

--- extract-installer.sh
+++ extract-installer.sh
@@ -10,6 +10,7 @@ docker run -i --rm -v `pwd`/tws:/tws debian sh -c " \
     && echo '75f2c2071bfc342932d6bca4ec321783ecb115cb99d23290fa254a2b87200c63  tws-installer.sh' | tee tws-installer.sh.sha256 \
     && curl -qL https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-linux-x64.sh -o tws-installer.sh \
     && yes '' | sh tws-installer.sh \
+    && rm -f /root/Jts/*/uninstall \
     && cp -r /root/Jts /tws"
 
 # && sha256sum -c tws-installer.sh.sha256 \
