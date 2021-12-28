4deafad347902586bd58215a240d012317eb0c9c
Brenden Matthews <brenden@brndn.io>
2021-12-28 08:49:07 -0600

    Bump TWS version.

--- extract-installer.sh
+++ extract-installer.sh
@@ -7,7 +7,7 @@ mkdir -p tws
 docker run -i --rm -v `pwd`/tws:/tws debian sh -c " \
     apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -qy --no-install-recommends unzip curl ca-certificates \
-    && echo '75f2c2071bfc342932d6bca4ec321783ecb115cb99d23290fa254a2b87200c63  tws-installer.sh' | tee tws-installer.sh.sha256 \
+    && echo '45657c8a48c5d477a17e37e37aee3f0b7ba041fed17e6068253bef03c6b5d772  tws-installer.sh' | tee tws-installer.sh.sha256 \
     && curl -qL https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-linux-x64.sh -o tws-installer.sh \
     && yes '' | sh tws-installer.sh \
     && rm -f /root/Jts/*/uninstall \
