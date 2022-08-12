11f285e3ddce21e1b03fe396d0fd088f7c526072
Brenden Matthews <brenden@brndn.io>
2022-08-12 11:58:56 -0500

    Bump dependencies for Python 3.10, fix import.

--- thetagang/dict_merge.py
+++ thetagang/dict_merge.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import collections
+from collections.abc import Mapping
 
 
 def dict_merge(dct, merge_dct):
