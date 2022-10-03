483aad8463fea2277d77efc4fd92f32aa5b63fc9
Brenden Matthews <brenden@brndn.io>
2022-10-03 11:26:19 -0500

    Allow algo to be configured.
    Also fixed some config default issues.

--- thetagang/config_defaults.py
+++ thetagang/config_defaults.py
@@ -1,4 +1,10 @@
 DEFAULT_CONFIG = {
+    "orders": {
+        "algo": {
+            "strategy": "Adaptive",
+            "params": [("adaptivePriority", "Patient")],
+        },
+    },
     "target": {
         "maximum_new_contracts_percent": 0.05,
         "delta": 0.3,
@@ -6,7 +12,7 @@ DEFAULT_CONFIG = {
     "write_when": {"puts": {"red": False}, "calls": {"green": False}},
     "roll_when": {
         "min_pnl": 0.0,
-        "close_at_pnl": None,
+        "close_at_pnl": 1.0,
         "calls": {"itm": True},
         "puts": {"itm": False},
     },
