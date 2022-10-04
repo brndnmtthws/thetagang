719771d307a50abc3805a01ecd58db4e8f2603d3
Brenden Matthews <brenden@brndn.io>
2022-10-04 10:51:26 -0500

    Need to divide by multiplier here.

--- thetagang/portfolio_manager.py
+++ thetagang/portfolio_manager.py
@@ -893,7 +893,7 @@ class PortfolioManager:
                             [strike_limit or sys.float_info.max]
                             + [
                                 position.contract.strike
-                                + position.averageCost
+                                + (position.averageCost / position.contract.multiplier)
                                 - midpoint_or_market_price(buy_ticker)
                             ]
                         ),
