41eb48e7fe85a6fa5d6d60cc37c8c039ca4f1219
Brenden Matthews <brenden@brndn.io>
2023-01-26 14:33:08 -0500

    Update portfolio_manager.py

--- thetagang/portfolio_manager.py
+++ thetagang/portfolio_manager.py
@@ -1138,7 +1138,7 @@ class PortfolioManager:
             return (
                 self.wait_for_midpoint_price(
                     ticker, wait_time=5
-                ) # need to keep the wait time relatively short to avoid blocking on slow stuff             
+                )  # need to keep the wait time relatively short to avoid blocking on slow stuff             
                 and midpoint_or_market_price(ticker) > minimum_price
             )
 
