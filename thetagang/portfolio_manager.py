0749b8681cd03b647f8db31a020e17406e0db511
Brenden Matthews <github@brenden.brndn.io>
2024-04-23 14:19:08 -0400

    Did not mean to do this

--- thetagang/portfolio_manager.py
+++ thetagang/portfolio_manager.py
@@ -2275,9 +2275,7 @@ class PortfolioManager:
                     # We only want to tighten spreads, not widen them. If the
                     # resulting price change would increase the spread, we'll
                     # skip it.
-                    if contract.secType == "BAG" and would_increase_spread(
-                        order, updated_price
-                    ):
+                    if would_increase_spread(order, updated_price):
                         console.print(
                             f"[yellow]Skipping order for {contract.symbol}"
                             f" with old lmtPrice={dfmt(order.lmtPrice)} updated lmtPrice={dfmt(updated_price)}, because updated price would increase spread"
